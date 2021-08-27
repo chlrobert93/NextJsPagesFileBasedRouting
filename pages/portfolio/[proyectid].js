ed5f096c81499932f4359b5a7198a2ddfb5a883c
Eder <chlrobert93@gmail.com>
2021-08-27 16:27:26 -0500

    Etracción de datos de segmento de ruta dinámica

--- pages/portfolio/[proyectid].js
+++ pages/portfolio/[proyectid].js
@@ -1,5 +1,13 @@
-function PorfolioProjectPage() {
+import { useRouter } from "next/router";
 
+function PorfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  /*enviar una solicitud a algún servidor backend
+para recuperar el dato con un id de*/
     return (
         <div>
             <h1>The Porfolio Proyect Page</h1>
