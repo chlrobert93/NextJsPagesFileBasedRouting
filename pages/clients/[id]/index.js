43d3ad996c3b1cc1e01ef6e6725ce6e7144e7646
Eder <chlrobert93@gmail.com>
2021-08-30 14:44:10 -0500

    Navegación programática

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -6,9 +6,19 @@ function ClientsProyectsPage() {
 
     console.log(router.query);
 
+    function loadProjectHandler() {
+         /*//Load data
+         router.push('/clients/max/projecta');*/
+         router.push({
+             pathname: '/clients/[id]/[clientprojectid]',
+             query: { id: 'max', clientprojectid: 'projecta'},
+         });
+    }
+
     return (
         <div>
             <h1>The Project of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load Proyect A</button>
         </div>
     );
 }
